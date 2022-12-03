3d0d4a5e941d5f8a9050367411ef7c5ae34c1642
pomu <dailysentence1111@gmail.com>
2022-12-03 23:56:54 +0900

    なんだっけ、えっと、とりあえず、viewコピペ

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.contrib.auth import get_user_model
+from django.views.genric import CreateView
 # Create your views here.
+
+User = get_user_model()
+
+class SignUpView(CreateView)
