ee7c74a0dadfb45f9ac24380cf103936b236a03c
pomu <dailysentence1111@gmail.com>
2022-12-17 17:47:22 +0900

    chotto

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model, login, authenticate  # なぜか順番変えたらエラーになった
+from django.contrib.auth import get_user_model, login, authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
