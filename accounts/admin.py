a4e50d86b0c1992fb08b375ce2cc98dfe99203e9
pomu <dailysentence1111@gmail.com>
2022-12-17 16:29:52 +0900

    admin手直しbase.html増やした

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 
 
 class UserAdmin(admin.ModelAdmin):
