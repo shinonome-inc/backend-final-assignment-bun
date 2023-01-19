7611767f6ddd0fe4840d6d75b5aaee151c46d012
khimoo <b.hibino@4nonome.com>
2023-01-19 18:13:00 +0900

    test以外おｋ

--- tweets/views.py
+++ tweets/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import TemplateView, View
 
 from tweets.forms import TweetCreateForm
-from tweets.models import Like, Tweet
+from tweets.models import Tweet
 
 # Create your views here.
 
@@ -11,12 +11,8 @@ from tweets.models import Like, Tweet
 class HomeView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         tweets = Tweet.objects.select_related("user").all()
-        liked_tweets = [
-            like_record.tweet for like_record in Like.objects.filter(user=request.user)
-        ]
         context = {
             "tweet": tweets,
-            "liked_tweets": liked_tweets,
         }
         return render(request, "tweets/home.html", context)
 
@@ -42,10 +38,8 @@ class TweetDetailView(LoginRequiredMixin, View):
             pk=kwargs.get("pk"),
         )
         is_tweet_user = tweet.user == request.user
-        like_count = Like.objects.filter(tweet=tweet).count()
         context = {
             "tweet": tweet,
-            "like_count": like_count,
             "is_tweet_user": is_tweet_user,
         }
         return render(
@@ -65,29 +59,3 @@ class TweetDeleteView(LoginRequiredMixin, View):
             return HttpResponseForbidden()
         tweet.delete()
         return redirect("welcome:home")
-
-
-class LikeView(LoginRequiredMixin, View):
-    def post(self, request, *args, **kwargs):
-        tweet = get_object_or_404(
-            Tweet,
-            pk=kwargs.get("pk"),
-        )
-        if Like.objects.filter(user=request.user, tweet=tweet).exists():
-            return HttpResponse("UNIQUE constraint failed", status=200)
-        Like.objects.create(user=request.user, tweet=tweet)
-        params = {"tweet": tweet.id}
-        return JsonResponse(params, status=200)
-
-
-class UnlikeView(LoginRequiredMixin, View):
-    def post(self, request, *args, **kwargs):
-        tweet = get_object_or_404(
-            Tweet,
-            pk=kwargs.get("pk"),
-        )
-        if not (like_record := Like.objects.filter(user=request.user, tweet=tweet)):
-            return HttpResponse("Like Record Unexist", status=200)
-        like_record.delete()
-        params = {"data": tweet.id}
-        return JsonResponse(params, status=200)
